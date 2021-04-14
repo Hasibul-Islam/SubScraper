a6b6d6147f69255af6de0b3080f0b2d2d61cab71
Md. Hasibul Islam <polok.hasibul@gmail.com>
2021-04-14 19:38:12 +0600

    testing

--- SubScraper/settings.py
+++ SubScraper/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -126,3 +126,6 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+django_heroku.settings(locals())
